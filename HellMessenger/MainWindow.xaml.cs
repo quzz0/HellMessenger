f0012aa4642fc7edf19ea5ad440e2feac13b39b8
quzz0 <quz0zq@gmail.com>
2015-10-04 01:16:19 +0300

    Added simple send panel

--- HellMessenger/MainWindow.xaml.cs
+++ HellMessenger/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace HellMessenger
 {
@@ -20,9 +8,24 @@ namespace HellMessenger
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        //WindowController controller = new WindowController();
+
         public MainWindow()
         {
             InitializeComponent();
+
+
+            Button sendButton = new Button();
+            sendButton.Content = "Test";
+            DockPanel.SetDock(sendButton, Dock.Right);
+
+            TextBox messageBox = new TextBox();
+            messageBox.TextWrapping = TextWrapping.Wrap;
+            messageBox.AcceptsReturn = true;
+
+            sendPanel.Children.Add(sendButton);
+            sendPanel.Children.Add(messageBox);
         }
     }
 }
